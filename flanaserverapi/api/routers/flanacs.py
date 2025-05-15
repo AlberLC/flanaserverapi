3f1da1fc4ee53718d91324c59f1a4caa2739933e
AlberLC <alberlc@outlook.com>
2025-05-15 12:29:49 +0200

    Add flanacs_old

--- flanaserverapi/api/routers/flanacs.py
+++ flanaserverapi/api/routers/flanacs.py
@@ -9,3 +9,8 @@ router = APIRouter(prefix='/flanacs', tags=['flanacs'])
 @router.get('/download')
 async def download() -> FileResponse:
     return FileResponse(config.flanacs_zip_path, filename=config.flanacs_zip_name)
+
+
+@router.get('/download/old')
+async def download_old() -> FileResponse:
+    return FileResponse(config.flanacs_zip_path_old, filename=config.flanacs_zip_name)
--- flanaserverapi/config.py
+++ flanaserverapi/config.py
@@ -10,9 +10,10 @@ class Config(BaseSettings):
     flanacs_path: Path = static_path / 'flanacs'
     flanatrigo_path: Path = static_path / 'flanatrigo'
     flanacs_zip_name: str = 'FlanaCS.zip'
-    flanacs_zip_path: Path = flanacs_path / 'FlanaCS.zip'
+    flanacs_zip_path: Path = flanacs_path / flanacs_zip_name
+    flanacs_zip_path_old: Path = flanacs_path / 'FlanaCS_old.zip'
     flanatrigo_zip_name: str = 'FlanaTrigo.zip'
-    flanatrigo_zip_path: Path = flanatrigo_path / 'FlanaTrigo.zip'
+    flanatrigo_zip_path: Path = flanatrigo_path / flanatrigo_zip_name
     flanatrigo_version_path: Path = flanatrigo_path / 'version.txt'
 
     model_config = SettingsConfigDict(env_file='../.env')
