283713b4d138239024a344f4eb59b6fef2b3f5f4
AlberLC <alberlc@outlook.com>
2026-02-12 21:06:27 +0100

    Fix ping_router.ping status code

--- flanaserverapi/api/routers/ping_router.py
+++ flanaserverapi/api/routers/ping_router.py
@@ -1,8 +1,8 @@
-from fastapi import APIRouter, Response, status
+from fastapi import APIRouter, status
 
 router = APIRouter(tags=['ping'])
 
 
-@router.get('/ping')
-async def ping() -> Response:
-    return Response(status_code=status.HTTP_204_NO_CONTENT)
+@router.get('/ping', status_code=status.HTTP_204_NO_CONTENT)
+async def ping() -> None:
+    pass
